6bb93ec2c9d758044dafb037075e2f5b8078da95
abbasian <abbasian@thn.com>
2025-05-07 08:11:59 +0330

    work on tailwind configs

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+const withOpacity = (variable: string) => {
+  return ({ opacityValue }: { opacityValue?: number }) =>
+    opacityValue !== undefined
+      ? `rgba(var(${variable}), ${opacityValue})`
+      : `rgb(var(${variable}))`
+}
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: withOpacity('--color-primary'),
+        secondary: withOpacity('--color-secondary'),
+        success: withOpacity('--color-success'),
+        warning: withOpacity('--color-warning'),
+        error: withOpacity('--color-error'),
+        background: withOpacity('--background-app-rgb'),
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
+  },
+  plugins: [],
+}
