63797ce897e26f2c9143d03a2edfd19e96116096
abbasian <abbasian@thn.com>
2025-05-09 12:03:38 +0330

    work on postgress DB

---
+++ backend/prisma/migrations/20250509074944_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Place" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Place_pkey" PRIMARY KEY ("id")
+);
