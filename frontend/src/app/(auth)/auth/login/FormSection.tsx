9a2aa549797dbb6ad1deb7115746744bd9848d2e
abbasian <abbasian@thn.com>
2025-10-19 18:53:46 +0330

    work on login and signup page

--- frontend/src/app/(auth)/auth/login/FormSection.tsx
+++ frontend/src/app/(auth)/auth/login/FormSection.tsx
@@ -7,33 +7,15 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { BsPersonAdd } from 'react-icons/bs'
-import { SignupInterface } from '@/@types/services/auth'
-import { useSignupMutation } from '@/services/auth'
+import { LoginInterface, SignupInterface } from '@/@types/services/auth'
 
 export const signupSchema = yup.object({
-  name: yup
-    .string()
-    .required('name field is required')
-    .min(2, 'Name must be at least 2 characters')
-    .max(30, 'Name must be maximum 30 characters'),
-
   email: yup
     .string()
     .required('email field is required')
     .email('Email must be valid'),
 
-  password: yup
-    .string()
-    .required('password field is required')
-    .min(8, 'Password must be at least 8 characters')
-    .max(30, 'Password must be maximum 30 characters')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/\d/, 'Password must contain at least one number')
-    .matches(
-      /[^a-zA-Z0-9]/,
-      'Password must contain at least one special character'
-    ),
+  password: yup.string().required('password field is required'),
 })
 
 function FormSection() {
@@ -45,14 +27,7 @@ function FormSection() {
     resolver: yupResolver(signupSchema),
   })
 
-  //API
-  const [
-    SignupMutation,
-    { isLoading: SignupMutationDataLoading, isError: SignupMutationError },
-  ] = useSignupMutation()
-
-  const sendDataHandler = async (data: SignupInterface) => {
-    await SignupMutation(data)
+  const sendDataHandler = (data: LoginInterface) => {
     console.log(data)
   }
   return (
@@ -61,13 +36,6 @@ function FormSection() {
       className="flex flex-col justify-center items-center w-full gap-10 bg-white/50 backdrop-blur-2xl rounded-lg p-4"
     >
       <BsPersonAdd className="w-36 h-36 text-primary" />
-      <FormInput
-        register={register}
-        name="name"
-        type="text"
-        placeholder="enter your name..."
-        error={errors.name}
-      />
       <FormInput
         register={register}
         name="email"
@@ -83,12 +51,12 @@ function FormSection() {
         error={errors.password}
       />
       <ButtonComp
-        text="sign up"
+        text="login"
         type="submit"
         className="bg-primary w-full mt-10"
       />
       <div className="flex justify-end w-full">
-        <LinkText href="/auth/login" text="login" />
+        <LinkText href="/auth/signup" text="sign up" />
       </div>
     </form>
   )
---
+++ frontend/src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormSection from './FormSection'
+
+const Login = () => {
+  return (
+    <div className="max-w-lg mx-auto">
+      <FormSection />
+    </div>
+  )
+}
+
+export default Login
